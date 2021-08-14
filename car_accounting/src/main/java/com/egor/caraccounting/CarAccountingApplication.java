9ef56181c57acaae9e99afb1dfb66745af7c9eb8
EgorKozlov <57860919+Soviet43585@users.noreply.github.com>
2021-08-14 18:10:45 +0300

    Add files via upload

---
+++ car_accounting/src/main/java/com/egor/caraccounting/CarAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.egor.caraccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAccountingApplication.class, args);
+	}
+
+}
---
+++ car_accounting/src/test/java/com/egor/caraccounting/CarAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egor.caraccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
